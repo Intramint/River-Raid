af94bca0b639a84d6cd28e8b957d21c90840b230
Intramint <marcinwieczorek8@gmail.com>
2026-09-08 15:35:27 +0200

    the first river segment now spawns. fixed a bug where changing walls collision resource would apply to all scene instances instead of one

--- scripts/river_segment.gd
+++ scripts/river_segment.gd
@@ -9,7 +9,7 @@ func create(top_left: Vector2, top_right: Vector2, bottom_right: Vector2, bottom
 	polygon.append(bottom_right)
 	polygon.append(bottom_left)
 	$Polygon2D.polygon = polygon
-	$LeftWallCollision.shape.a = bottom_left
+	$LeftWallCollision.shape.a = bottom_left #	$LeftWallCollision.shape = $LeftWallCollision.shape.duplicate() would be necessary but the alternative is to set "Local to Scene" in the Resource of the two collision nodes
 	$LeftWallCollision.shape.b = top_left
 	$RightWallCollision.shape.a = bottom_right
 	$RightWallCollision.shape.b = top_right
--- scripts/river_spawner.gd
+++ scripts/river_spawner.gd
@@ -1,11 +1,11 @@
-extends Node
+extends Node2D
 
 @onready var segment_height: float = abs($TopLeftPoint.position.y - $BottomLeftPoint.position.y)
 var distance_since_last_segment: float = 0.0
 
 
 func _ready() -> void:
-	spawn_segment($TopLeftPoint.position, $TopRightPoint.position, $BottomRightPoint.position, $BottomLeftPoint.position)
+	spawn_first_segment()
 
 
 func _physics_process(delta: float) -> void:
@@ -23,3 +23,10 @@ func spawn_segment(top_left: Vector2, top_right: Vector2, bottom_right: Vector2,
 	var segment: RiverSegment = load("res://scenes/river_segment.tscn").instantiate()
 	segment.create(top_left, top_right, bottom_right, bottom_left)
 	add_child(segment)
+
+func spawn_first_segment():
+	var bottom_left: Vector2 = $BottomLeftPoint.position
+	bottom_left.y = get_viewport_rect().size.y
+	var bottom_right: Vector2 = $BottomRightPoint.position
+	bottom_right.y = get_viewport_rect().size.y
+	spawn_segment($TopLeftPoint.position, $TopRightPoint.position, bottom_right, bottom_left)
