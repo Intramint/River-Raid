a6854af4f3284624217e5d4e37738640d71850d2
Intramint <marcinwieczorek8@gmail.com>
2026-09-08 13:36:22 +0200

    added game_state

--- scripts/components/scrollable.gd
+++ scripts/components/scrollable.gd
@@ -1,8 +1,6 @@
 extends Node
 class_name Scrollable
 
-const SCROLL_SPEED: float = 200
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	get_parent().global_position.y += SCROLL_SPEED * delta
+	get_parent().global_position.y += GameState.SCROLL_SPEED * delta
