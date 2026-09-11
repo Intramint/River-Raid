4be8a24c6fb72e1b9c3f2073a1de83a227755028
Intramint <marcinwieczorek8@gmail.com>
2026-09-11 13:54:37 +0200

    added hurtbox component

---
+++ scripts/components/hurtbox.gd
@@ -0,0 +1,11 @@
+extends Area2D
+class_name Hurtbox
+
+signal hurt(by: Node2D)
+
+func _on_area_entered(area: Area2D) -> void:
+	hurt.emit(area)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hurt.emit(body)
